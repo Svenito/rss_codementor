3f70c97ca179df4c5be616e6af5e0450f76619df
Sven Steinbauer <sven@unlogic.co.uk>
2021-03-09 17:07:29 +0000

    more heroku configs

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,10 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :rss, RssWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  # url: [host: "example.com", port: 80],
+  http: [port: {:system, "PORT"}],
+  url: [scheme: "https", host: "stormy-ridge-70328.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
--- lib/rss_web/endpoint.ex
+++ lib/rss_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule RssWeb.Endpoint do
   ]
 
   socket "/socket", RssWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],,
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
