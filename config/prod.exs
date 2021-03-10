35ac3afb75f63ffe5b209bb6c271c21e6586b77e
Sven Steinbauer <sven@unlogic.co.uk>
2021-03-10 12:45:29 +0000

    setup production deploys

--- config/prod.exs
+++ config/prod.exs
@@ -56,3 +56,10 @@ config :logger, level: :info
 # Finally import the config/prod.secret.exs which loads secrets
 # and configuration from environment variables.
 import_config "prod.secret.exs"
+
+config :rss, Rss.Endpoint,
+  # Possibly not needed, but doesn't hurt
+  http: [port: {:system, "PORT"}],
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  server: true
